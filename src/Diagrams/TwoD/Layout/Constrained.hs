c789cd467216b883a32826bfb59c13bddc261a41
Brent Yorgey <byorgey@gmail.com>
2015-08-24 16:01:44 -0500

    WIP: progress on 'intro'

--- src/Diagrams/TwoD/Layout/Constrained.hs
+++ src/Diagrams/TwoD/Layout/Constrained.hs
@@ -24,6 +24,7 @@ import           Math.MFSolve
 import           Diagrams.Coordinates
 import           Diagrams.Prelude     (QDiagram)
 import           Diagrams.TwoD        (P2, V2, unitX, unitY, unit_X, unit_Y)
+import           Linear.Affine
 import           Linear.Vector
 
 type Handle = Int
@@ -55,19 +56,39 @@ mkSystem = flip solveEqs
 --       => P2 (Expr SimpleVar n) -> P2 (Expr SimpleVar n) -> System n
 -- can't use the above since Phase is not exported by mfsolve
 
+infix 4 =.=
 (=.=) :: P2 (Expr SimpleVar Double) -> P2 (Expr SimpleVar Double) -> System Double
 (coords -> px :& py) =.= (coords -> qx :& qy) = mkSystem [ px === qx, py === qy ]
 
-intro :: QDiagram b v n m -> Constrained b v n m Handle
+intro :: QDiagram b v Double m -> Constrained b v Double m Handle
 intro dia = do
   v <- varCounter <+= 1
   diagrams %= (\m -> m & at v .~ Just (dia, False))
+  forM_ dirs $ \(dirName, dirV) ->
+    constrain $ ccenter v .+^ dirV =.= subpoint dirName v
 
   undefined -- for each direction in dirs, look up envelope and set up
             -- constraint: diagram x/y are fixed distance away from
             -- the given directional points.
 
-intros :: [QDiagram b v n m] -> Constrained b v n m [Handle]
+subvar :: String -> Handle -> Expr SimpleVar Double
+subvar s h = makeVariable . SimpleVar $ show h ++ "_" ++ s
+
+-- TODO: use a data type for these e.g. var =~ (Handle, Name, XY)
+
+subpoint :: String -> Handle -> P2 (Expr SimpleVar Double)
+subpoint s h = subvar (s ++ "_x") h ^& subvar (s ++ "_y") h
+
+cx :: Handle -> Expr SimpleVar Double
+cx = subvar "center_x"
+
+cy :: Handle -> Expr SimpleVar Double
+cy = subvar "center_y"
+
+ccenter :: Handle -> P2 (Expr SimpleVar Double)
+ccenter h = cx h ^& cy h
+
+intros :: [QDiagram b v Double m] -> Constrained b v Double m [Handle]
 intros = mapM intro
 
 introScaled :: QDiagram b v n m -> Constrained b v n m Handle
